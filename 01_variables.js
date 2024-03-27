fa4ce12e31d7d04ea19f17bce78390546b12798e
MAJHI <122159208+majhintech@users.noreply.github.com>
2024-03-27 17:28:57 +0300

    datatypes

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 123456;
+let accountEmail = "majhi@gmail.com";
+var accountPw = "123456ggd";
+
+accountCity = "Jaipur";
+
+let accountState;
+
+// accountId = 2;
+
+console.log(accountId);
+
+console.table([accountEmail, accountPw, accountCity, accountState]);
---
+++ 02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+// alert(`Hey `)  //we are using nodejs, not browser
+
+let name = "majhi";
+let age = 18;
+let isLoggedIn = false;
+
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => uniqe
+
+// object
+
+console.log(typeof undefined);
