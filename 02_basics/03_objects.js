bf1fa91d89ef2e026610f0ec1e41c1233aa51f3a
MAJHI <122159208+majhintech@users.noreply.github.com>
2024-03-28 17:54:25 +0300

    learning objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,22 @@
+//  singleton
+
+// object literals
+
+// Object.create // constractor
+
+// const mySym = symbol("key1");
+
+const jsUser = {
+  name: "majhintech",
+  age: 18,
+  location: "saudi arabia",
+  email: "majhintech@gmail.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Saturday"],
+};
+
+console.log(jsUser["email"]);
+
+console.log(jsUser["full name"]);
+
+console.log(jsUser["full name"]);
